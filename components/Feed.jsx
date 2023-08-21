dac2cee90f0584fcfc4cb42e55fe46bde6349b32
ycl818 <kevinyichienlee@gmail.com>
2023-08-21 17:53:57 +0800

    ready to show feed

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,42 @@
-import React from "react";
+"use client";
+
+import { useState, useEffect } from "react";
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  <div className="mt-16 prompt_layout"></div>;
+};
 
 // show prompts
 const Feed = () => {
-  return <div>Kevin5_lee == ycl818</div>;
+  const [searchText, setSearchText] = useState("");
+  const [posts, setPosts] = useState([]);
+  const handleSearchChange = (e) => {};
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch("/apu/prompt");
+      const data = await response.json();
+      setPosts(data);
+    };
+
+    fetchPosts();
+  }, []);
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+
+      <PromptCardList data={posts} handleTagClick={() => {}} />
+    </section>
+  );
 };
 
 export default Feed;
