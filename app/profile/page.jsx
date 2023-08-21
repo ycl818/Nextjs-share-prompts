ce42d041841e7d4fce6ef48ef1a18b4561edf6bb
yichien <kevinyichienlee@gmail.com>
2023-08-22 01:06:44 +0800

    delete post

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -27,12 +27,29 @@ const MyProfile = () => {
     router.push(`/update-prompt?id=${post._id}`);
   };
 
-  const handleDelete = () => {};
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm(
+      "Are you sure you want to delete this prompt?"
+    );
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: "DELETE",
+        });
+
+        const filteredPosts = posts.filter((item) => item._id !== post._id);
+
+        setPosts(filteredPosts);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
 
   return (
     <Profile
-      name="my"
-      desc="welcome to your personalized profile page"
+      name="My"
+      desc="Welcome to your personalized profile page"
       data={posts}
       handleEdit={handleEdit}
       handleDelete={handleDelete}
